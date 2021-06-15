0772e1040891106a799286d6a71ba4b453a19a56
Ashok812 <chhetriarash81@gmail.com>
2021-06-15 14:23:00 +0545

    Media Setup Complete

---
+++ app/Services/MediaService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Media;
+
+use Illuminate\Support\Str;
+
+class MediaService {
+    /**
+     * Upload Service for All Models
+     *
+     * @param $file
+     * @param string $folder
+     * @param string $name
+     * @param false $thumbnail_required
+     * @param bool $public
+     * @return false|int
+     */
+
+    public static function upload($file, $folder="uploads",$name="",$thumbnail_required= false, $public= true )
+    {
+        if(empty($name))
+            $name = Str::slug($file->getClientOriginalName());
+
+        $type = $file->getMimeType();
+
+        $ext= $file->extension();
+
+        $folderPath = $folder;
+
+        if($public)
+            $folderPath = "public/".$folder;
+
+        $filePath = Str::random(10).mt_rand(1,100).".".$ext;
+
+        $file->storeAs($folderPath, $filePath);
+
+        $media = Media::create([
+            'name'  => $name,
+            'path'  => $folder."/".$filePath,
+            "type"  =>  $type
+        ]);
+
+        return $media->id;
+
+    }
+}
