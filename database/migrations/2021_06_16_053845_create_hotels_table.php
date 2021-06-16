da63656548699c750eb9d534263042a9d9918e49
Ashok812 <chhetriarash81@gmail.com>
2021-06-16 11:26:07 +0545

    Hotel Model and Migration Created

---
+++ database/migrations/2021_06_16_053845_create_hotels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('media_id')->nullable();
+            $table->string('address');
+            $table->longText('body')->nullable();
+            $table->string('rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+}
