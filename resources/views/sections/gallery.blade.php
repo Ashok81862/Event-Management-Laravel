9041e6c87e76ae11f3c5b2be60959956f8d6a739
Ashok812 <chhetriarash81@gmail.com>
2021-06-16 19:05:05 +0545

    Gallery Dynamic Created

--- resources/views/sections/gallery.blade.php
+++ resources/views/sections/gallery.blade.php
@@ -6,57 +6,21 @@
         <p>Check our Gallery</p>
       </header>
       <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
-        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-          <div class="portfolio-wrap">
-            <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>App 3</h4>
-              <p>App</p>
-              <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 3"><i class="bi bi-plus"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-          <div class="portfolio-wrap">
-            <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>App 3</h4>
-              <p>App</p>
-              <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 3"><i class="bi bi-plus"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-          <div class="portfolio-wrap">
-            <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>App 3</h4>
-              <p>App</p>
-              <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 3"><i class="bi bi-plus"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-          <div class="portfolio-wrap">
-            <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>App 3</h4>
-              <p>App</p>
-              <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 3"><i class="bi bi-plus"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
+       @foreach ($galleries as $gallery)
+           @if($gallery->media_id)
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <div class="portfolio-wrap">
+                    <img src="/storage/{{$gallery->media->path }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>{{ $gallery->name }}</h4>
+                        <div class="portfolio-links">
+                        <a href="/storage/{{$gallery->media->path }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="{{ $gallery->name }}"><i class="bi bi-plus"></i></a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+           @endif
+       @endforeach
       </div>
 
 
